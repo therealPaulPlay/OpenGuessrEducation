29d90a2a320446c013d8491aef134f333e55c774
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2024-09-28 16:58:40 +0200

    improve start page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import Globe from "$lib/components/Globe.svelte";
     import { Users, MapPin, Info } from "lucide-svelte";
+    import DollarSign from "$lib/components/DollarSign.svelte";
     import { onMount } from "svelte";
 
     let progress = 75;
@@ -63,36 +64,32 @@
         </div>
     </div>
 
-    <div class="stats shadow w-full mb-12 bg-base-200">
-        <div class="stat">
-            <div class="stat-figure text-primary">
-                <Users size={36} />
-            </div>
-            <div class="stat-title">Active Players</div>
-            <div class="stat-value text-primary">800.000+</div>
-            <div class="stat-desc">Learn from real Gigachads.</div>
+<div class="stats shadow w-full mb-12 bg-base-200">
+    <div class="stat">
+        <div class="stat-figure text-primary">
+            <Users size={36} />
         </div>
-
-        <div class="stat">
-            <div class="stat-figure text-secondary">
-                <MapPin size={36} />
-            </div>
-            <div class="stat-title">Countries Covered</div>
-            <div class="stat-value text-secondary">196</div>
-            <div class="stat-desc">Out of 196 recognized countries</div>
-        </div>
-
-        <div class="stat">
-            <div class="stat-figure text-secondary">
-                <div class="avatar">
-                    <div class="w-16 rounded-full">
-                        <img src="/api/placeholder/64/64" alt="User Avatar" />
-                    </div>
-                </div>
-            </div>
-            <div class="stat-value">86%</div>
-            <div class="stat-title">Tasks completed</div>
-            <div class="stat-desc text-secondary">31 tasks remaining</div>
-        </div>
+        <div class="stat-title">Active Players</div>
+        <div class="stat-value text-primary">800.000+</div>
+        <div class="stat-desc">Learn from a large community.</div>
     </div>
+
+    <div class="stat">
+        <div class="stat-figure text-secondary">
+            <MapPin size={36} />
+        </div>
+        <div class="stat-title">Countries Covered</div>
+        <div class="stat-value text-secondary">83</div>
+        <div class="stat-desc">Out of 83 countries with streetview</div>
+    </div>
+
+    <div class="stat">
+        <div class="stat-figure text-secondary">
+            <DollarSign size={36} />
+        </div>
+        <div class="stat-title">Cost</div>
+        <div class="stat-value text-secondary">Free</div>
+        <div class="stat-desc">OpenGuessr is commited to being free</div>
+    </div>
+</div>
 </div>
