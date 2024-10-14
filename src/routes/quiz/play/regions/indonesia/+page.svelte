cb10c6d0f5db8e02564fb25791879f2538d033d1
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2024-10-15 01:22:04 +0200

    add more region quizzes

---
+++ src/routes/quiz/play/regions/indonesia/+page.svelte
@@ -0,0 +1,14 @@
+<script context="module">
+    import { determineCategory } from "$lib/utils/quizMetadataHelper.js";
+    import QuizMapGame from "$lib/components/QuizMapGame.svelte";
+
+    export const metadata = {
+        title: "Indonesian Provinces (Provinsi)",
+        tags: ["Asia"],
+        region: "Indonesian Provinces",
+        topoJson: "indonesian-provinces",
+        category: determineCategory(new URL(import.meta.url).pathname)
+    }
+</script>
+
+<QuizMapGame region={metadata.region} topoJsonName={metadata.topoJson} />
---
+++ src/routes/quiz/play/regions/spain/+page.svelte
@@ -0,0 +1,14 @@
+<script context="module">
+    import { determineCategory } from "$lib/utils/quizMetadataHelper.js";
+    import QuizMapGame from "$lib/components/QuizMapGame.svelte";
+
+    export const metadata = {
+        title: "Spanish Regiones Autónomas",
+        tags: ["Europe"],
+        region: "Spanish Regions",
+        topoJson: "spanish-regions",
+        category: determineCategory(new URL(import.meta.url).pathname)
+    }
+</script>
+
+<QuizMapGame region={metadata.region} topoJsonName={metadata.topoJson} />
