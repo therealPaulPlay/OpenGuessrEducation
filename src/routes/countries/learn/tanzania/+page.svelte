77be979add5e7d8c6ff6c9ba04057f3d60499007
p3ell <141396000+p3ell@users.noreply.github.com>
2024-11-01 11:50:50 +1100

    Update +page.svelte
    added message about how it has no street view

--- src/routes/countries/learn/tanzania/+page.svelte
+++ src/routes/countries/learn/tanzania/+page.svelte
@@ -1,1 +1,4 @@
-<!-- Write or paste an article with HTML & Svelte here -->
+<script>
+</script>
+
+<p class="mb-2">Tanzania has no official Google Street View coverage, however it does have some unofficial coverage in the north in the National Parks.</p>
