f59cc5ddd1262f1f201961da96e56e4a32de1027
SirBastalot <155985550+SirBastalot@users.noreply.github.com>
2024-12-09 22:01:04 +0100

    Short article on denmark
    Some info on signs, and landscape

--- src/routes/countries/learn/denmark/+page.svelte
+++ src/routes/countries/learn/denmark/+page.svelte
@@ -1,1 +1,43 @@
-<!-- Write or paste an article with HTML & Svelte here -->
+<script>
+    import ArticleTip from "$lib/components/ArticleTip.svelte";
+    import ArticleEmbedPanorama from "$lib/components/ArticleEmbedPanorama.svelte";
+</script>
+
+<!-- main h1 header (only one per page) -->
+<p class="mb-2">
+    Denmark is located in Northern Europe and consists of the Jutland Peninsula and numerous islands, including Zealand, Funen, and Bornholm.
+    It is surrounded by the North Sea and the Baltic Sea.
+</p>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">Bollard</h2>
+<img
+    alt="street view example lining up"
+    src="/assets/guides/denmark/denmark_bollard.png"
+    class="rounded-xl w-1/3" />
+
+
+<p class="mt-2">
+    The bollard can be confused with the Belgian one, however it will never feature the <b>orange strip</b>!
+</p>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">Signs</h2>
+<div class="flex space-x-4"> 
+    <img alt="street view example lining up" src="/assets/guides/denmark/denmark_signpost.png" class="rounded-xl w-1/3" /> 
+    <img alt="street view example lining up" src="/assets/guides/denmark/denmark_directionsign.png" class="rounded-xl w-1/3" /> 
+</div>
+<p class="mt-2">
+    The metal post is wrapped around the sign. Directionsigns will always feature this unique style. 
+</p>
+<img alt="street view example lining up" src="/assets/guides/denmark/vej_sign.png" class="rounded-xl w-1/3" />     
+<p class="mt-2">
+    The ending <b>"vej"</b> is the equivelent of <b>"street"</b> in danish.
+</p>
+<h2 class="text-2xl font-bold mt-8 mb-2">Landscape</h2>
+
+<p class="mt-2">Denmark is noticeably flat, you will barly see any hills at all. The rural landscape consists mostly of green fiels.</p>
+<ArticleEmbedPanorama src="https://www.google.com/maps/embed?pb=!4v1733777624393!6m8!1m7!1sC5D6Q53lYImJqR9-1Xpy7Q!2m2!1d56.67743985281356!2d9.669265542420174!3f37.77!4f0.37000000000000455!5f0.7820865974627469" />
+
+
+<ArticleTip>
+    The images might look especially lush green since most coverage is Gen 4.
+</ArticleTip>
