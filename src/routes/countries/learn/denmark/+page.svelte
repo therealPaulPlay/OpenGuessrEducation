017777951788865a22e2b1ab8806bb1ab1fe2019
SirBastalot <155985550+SirBastalot@users.noreply.github.com>
2024-12-10 22:17:56 +0100

    Updated my denmark article
    Got rid of some issues and changes the usage of bold font.

--- src/routes/countries/learn/denmark/+page.svelte
+++ src/routes/countries/learn/denmark/+page.svelte
@@ -8,36 +8,46 @@
     Denmark is located in Northern Europe and consists of the Jutland Peninsula and numerous islands, including Zealand, Funen, and Bornholm.
     It is surrounded by the North Sea and the Baltic Sea.
 </p>
+<ArticleTip>
+    Keep in mind that, theoretically, both the <b>Faroe Islands</b> and <b>Greenland</b> are autonomous <b>territories</b> within the <b>Kingdom of Denmark</b>.
+</ArticleTip>
 
 <h2 class="text-2xl font-bold mt-8 mb-2">Bollard</h2>
 <img
     alt="street view example lining up"
-    src="/assets/guides/denmark/denmark_bollard.png"
+    src="/assets/articles/denmark/denmark_bollard.jpeg"
     class="rounded-xl w-1/3" />
 
 
-<p class="mt-2">
-    The bollard can be confused with the Belgian one, however it will never feature the <b>orange strip</b>!
+<p class="text-center mt-2">
+    The <b>bollard</b> will always feature a <b>orange strip at the top</b> unlike the <b>Belgian one</b>, which looks <b>similar</b>, but lacks this <b>strip</b>.
 </p>
+    
+    
 
 <h2 class="text-2xl font-bold mt-8 mb-2">Signs</h2>
 <div class="flex space-x-4"> 
-    <img alt="street view example lining up" src="/assets/guides/denmark/denmark_signpost.png" class="rounded-xl w-1/3" /> 
-    <img alt="street view example lining up" src="/assets/guides/denmark/denmark_directionsign.png" class="rounded-xl w-1/3" /> 
+    <img alt="street view example lining up" src="/assets/articles/denmark/denmark_signpost.jpeg" class="rounded-xl w-1/3" /> 
+    <img alt="street view example lining up" src="/assets/articles/denmark/denmark_directionsign.jpeg" class="rounded-xl w-1/3" /> 
 </div>
-<p class="mt-2">
-    The metal post is wrapped around the sign. Directionsigns will always feature this unique style. 
+<p class="text-center mt-2">
+    The <b>metal post</b> is <b>wrapped</b> around the <b>sign</b>. <b>Directionsigns</b> will always feature this <b>unique style</b>.
 </p>
-<img alt="street view example lining up" src="/assets/guides/denmark/vej_sign.png" class="rounded-xl w-1/3" />     
-<p class="mt-2">
-    The ending <b>"vej"</b> is the equivelent of <b>"street"</b> in danish.
+<br>
+<img alt="street view example lining up" src="/assets/articles/denmark/vej_sign.jpeg" class="rounded-xl w-1/3" />     
+<p class="text-center mt-2">
+    The ending <b>"vej" is </b>the equivelent of <b>"street"</b> in danish.
 </p>
 <h2 class="text-2xl font-bold mt-8 mb-2">Landscape</h2>
 
-<p class="mt-2">Denmark is noticeably flat, you will barly see any hills at all. The rural landscape consists mostly of green fiels.</p>
+<p class="mt-2">
+    <b>Denmark</b> is noticeably <b>flat</b>, you will barly see any <b>hills</b> at all. The <b>rural landscape</b> consists mostly of <b>green fields</b>.
+</p>
+
 <ArticleEmbedPanorama src="https://www.google.com/maps/embed?pb=!4v1733777624393!6m8!1m7!1sC5D6Q53lYImJqR9-1Xpy7Q!2m2!1d56.67743985281356!2d9.669265542420174!3f37.77!4f0.37000000000000455!5f0.7820865974627469" />
-
-
-<ArticleTip>
-    The images might look especially lush green since most coverage is Gen 4.
-</ArticleTip>
+<p class="text-center mt-2">The images might <b>look</b> especially lush <b>green since</b> most coverage is <b>Gen 4</b>.</p>
+<p class="text-center mt-2">
+    <a href="https://education.openguessr.com/guides/read/beginner/coverage" target="_blank" rel="noopener noreferrer">
+        (click here to learn more about camera generations).
+    </a>
+</p>
