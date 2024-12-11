28781460d53c00f35e55d178314c57b2d16acdfc
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2024-12-11 16:29:43 +0100

    fix denmark page

--- src/routes/countries/learn/denmark/+page.svelte
+++ src/routes/countries/learn/denmark/+page.svelte
@@ -5,7 +5,7 @@
 
 <!-- main h1 header (only one per page) -->
 <p class="mb-2">
-    Denmark is located in Northern Europe and consists of the Jutland Peninsula and numerous islands, including Zealand, Funen, and Bornholm.
+    Denmark is located in <b>Northern Europe</b> and consists of the Jutland Peninsula and numerous islands, including Zealand, Funen, and Bornholm.
     It is surrounded by the North Sea and the Baltic Sea.
 </p>
 <ArticleTip>
@@ -13,28 +13,28 @@
 </ArticleTip>
 
 <h2 class="text-2xl font-bold mt-8 mb-2">Bollard</h2>
-<img
+<div class="flex justify-center items-center">
+    <img
     alt="street view example lining up"
     src="/assets/articles/denmark/denmark_bollard.jpeg"
-    class="rounded-xl w-1/3" />
+    class="rounded-xl w-full h-96 object-cover" />
+</div>
 
 
 <p class="text-center mt-2">
-    The <b>bollard</b> will always feature a <b>orange strip at the top</b> unlike the <b>Belgian one</b>, which looks <b>similar</b>, but lacks this <b>strip</b>.
+    The <b>bollard</b> will always feature an <b>orange strip at the top</b> unlike the <b>Belgian one</b>, which lacks this <b>strip</b>.
 </p>
-    
-    
 
 <h2 class="text-2xl font-bold mt-8 mb-2">Signs</h2>
-<div class="flex space-x-4"> 
-    <img alt="street view example lining up" src="/assets/articles/denmark/denmark_signpost.jpeg" class="rounded-xl w-1/3" /> 
-    <img alt="street view example lining up" src="/assets/articles/denmark/denmark_directionsign.jpeg" class="rounded-xl w-1/3" /> 
+<div class="flex space-x-4 w-full"> 
+    <img alt="street view example lining up" src="/assets/articles/denmark/denmark_signpost.jpeg" class="rounded-xl w-1/2" /> 
+    <img alt="street view example lining up" src="/assets/articles/denmark/denmark_directionsign.jpeg" class="rounded-xl w-1/2" /> 
 </div>
 <p class="text-center mt-2">
     The <b>metal post</b> is <b>wrapped</b> around the <b>sign</b>. <b>Directionsigns</b> will always feature this <b>unique style</b>.
 </p>
 <br>
-<img alt="street view example lining up" src="/assets/articles/denmark/vej_sign.jpeg" class="rounded-xl w-1/3" />     
+<img alt="street view example lining up" src="/assets/articles/denmark/vej_sign.jpeg" class="rounded-xl w-full" />     
 <p class="text-center mt-2">
     The ending <b>"vej" is </b>the equivelent of <b>"street"</b> in danish.
 </p>
@@ -46,8 +46,5 @@
 
 <ArticleEmbedPanorama src="https://www.google.com/maps/embed?pb=!4v1733777624393!6m8!1m7!1sC5D6Q53lYImJqR9-1Xpy7Q!2m2!1d56.67743985281356!2d9.669265542420174!3f37.77!4f0.37000000000000455!5f0.7820865974627469" />
 <p class="text-center mt-2">The images might <b>look</b> especially lush <b>green since</b> most coverage is <b>Gen 4</b>.</p>
-<p class="text-center mt-2">
-    <a href="https://education.openguessr.com/guides/read/beginner/coverage" target="_blank" rel="noopener noreferrer">
-        (click here to learn more about camera generations).
-    </a>
-</p>
+
+<p class="italic mt-4">Written by IceB3r from the Discord Server</p>
