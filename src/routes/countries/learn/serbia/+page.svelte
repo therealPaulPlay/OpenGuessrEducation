1a29eb6c737b40124a9ecd6c93205830c388467a
SirBastalot <155985550+SirBastalot@users.noreply.github.com>
2024-12-11 18:37:27 +0100

    Serbia article
    Changed a picture of the denmark article and added stuff about serbia.

--- src/routes/countries/learn/serbia/+page.svelte
+++ src/routes/countries/learn/serbia/+page.svelte
@@ -1,1 +1,80 @@
-<!-- Write or paste an article with HTML & Svelte here -->
+<script>
+    import ArticleTip from "$lib/components/ArticleTip.svelte";
+    import ArticleEmbedPanorama from "$lib/components/ArticleEmbedPanorama.svelte";
+</script>
+
+
+<p class="mb-2">
+    <b>Serbia</b> is a landlocked country in Southeast Europe, situated at the crossroads of Central and Balkan Europe. It features diverse landscapes, including the <b>Pannonian Plain</b> to the north and mountainous regions to the south.
+    <br>
+    The country uses the <b>Cyrillic alphabet</b> as one of its official scripts.
+</p>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">Bollard</h2>
+<div class="flex space-x-4 w-full">
+    <img
+    alt="street view example lining up"
+    src="/assets/articles/serbia/bollard1.jpg"
+    class="rounded-xl w-full h-96 object-cover" />
+    <img
+    alt="street view example lining up"
+    src="/assets/articles/serbia/bollard2.jpg"
+    class="rounded-xl w-full h-96 object-cover" />
+</div>
+<p class="text-center mt-2">
+    Notice that the <b>bollards</b> are very slim, while they are much <b>thicker</b> in other countries.
+</p>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">Electricity Poles</h2>
+<div class="flex space-x-4 w-full">
+    <img
+    alt="street view example lining up"
+    src="/assets/articles/serbia/serbia_pole.jpg"
+    class="rounded-xl w-full h-96 object-cover" />
+    <img
+    alt="street view example lining up"
+    src="/assets/articles/serbia/serbia_pole2.jpg"
+    class="rounded-xl w-full h-96 object-cover" />
+</div>
+<p class="text-center mt-2">
+    Though there are other poles found in Serbia, you will most commonly encounter either a <b>round concrete</b> one or a <b>rusty metal</b> one.
+</p>
+
+<br>
+<ArticleTip>
+    Pro tip: While most surrounding countries' Google car has an antenna, Serbia's car usually does not!
+</ArticleTip>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">Housing</h2>
+<div class="flex space-x-4 w-full">
+    <img
+    alt="street view example lining up"
+    src="/assets/articles/serbia/polished_house.jpg"
+    class="rounded-xl w-full h-96 object-cover" />
+    <img
+    alt="street view example lining up"
+    src="/assets/articles/serbia/brickhouse.jpg"
+    class="rounded-xl w-full h-96 object-cover" />
+</div>
+<p class="text-center mt-2">
+    The most common types of houses. <b>Red brick walls</b> or <b>polished walls</b> in bright colors (usually plain white). Nearly all houses have a <b>red tiled roof</b>.
+</p>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">Roadlines</h2>
+<img
+alt="street view example lining up"
+src="/assets/articles/serbia/roadlines.jpg"
+class="rounded-xl w-full h-96 object-cover" />
+<p class="text-center mt-2">
+    On Serbian roads, noticeably often there is just a <b>middle line</b> visible. (Does not apply to major highways!)
+</p>
+<h2 class="text-2xl font-bold mt-8 mb-2">Guardrail</h2>
+<img
+alt="street view example lining up"
+src="/assets/articles/serbia/serbia_guardrail.jpg"
+class="rounded-xl w-full h-96 object-cover" />
+<p class="text-center mt-2">
+    Serbia exclusively uses the <b>B-Type guardrail</b>. Keep in mind that Croatia and Montenegro also use this type!
+</p>
+
+<p class="italic mt-4">Written by IceB3r from the Discord Server</p>
