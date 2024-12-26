42a64070707958e3f288886d7cef205abdb31d05
SirBastalot <155985550+SirBastalot@users.noreply.github.com>
2024-12-26 11:04:43 +0100

    Article on Bhutan

--- src/routes/countries/learn/indonesia/+page.svelte
+++ src/routes/countries/learn/indonesia/+page.svelte
@@ -1,1 +1,9 @@
 <!-- Write or paste an article with HTML & Svelte here -->
+<script>
+    import ArticleTip from "$lib/components/ArticleTip.svelte";
+    import ArticleEmbedPanorama from "$lib/components/ArticleEmbedPanorama.svelte";
+</script>
+
+
+<p class="mb-2"> <b>Indonesia</b> is an archipelagic country in Southeast Asia, located between the Indian and Pacific Oceans. It consists of over 17,000 islands, with diverse landscapes ranging from tropical rainforests to volcanic mountains.
+     <br> The country uses the <b>Indonesian language</b> as its official language. </p>
