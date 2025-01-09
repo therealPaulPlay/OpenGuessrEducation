616bbdd0b068cc663d29ccd64c8f15a82882c34a
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2025-01-09 17:31:21 +0100

    fixes for indonesia article

--- src/routes/countries/learn/indonesia/+page.svelte
+++ src/routes/countries/learn/indonesia/+page.svelte
@@ -1,70 +1,105 @@
 <script>
-    import ArticleTip from "$lib/components/ArticleTip.svelte";
-    import ArticleEmbedPanorama from "$lib/components/ArticleEmbedPanorama.svelte";
-    import { Currency } from "lucide-svelte";
     import Map from "$lib/components/Map.svelte";
 </script>
 
-<p class="mb-2"> <b>Indonesia</b> is an <b>archipelagic country</b> in <b>Southeast Asia</b>, located between the <b>Indian</b> and <b>Pacific Oceans</b>. It consists of over <b>17,000 islands</b>, with diverse landscapes ranging from <b>tropical rainforests</b> to <b>volcanic mountains</b>.
-<br> The country uses the <b>Indonesian language</b> as its <b>official language</b>. </p>
+<p class="mb-2">
+    <b>Indonesia</b> is an <b>archipelagic country</b> in <b>Southeast Asia</b>,
+    located between the <b>Indian</b> and <b>Pacific Oceans</b>. It consists of
+    over <b>17,000 islands</b>, with diverse landscapes ranging from
+    <b>tropical rainforests</b>
+    to <b>volcanic mountains</b>.The country uses the <b>Indonesian language</b>
+    as its <b>official language</b>.
+</p>
 
 <h2 class="text-2xl font-bold mt-8 mb-2">Licence plates</h2>
-<img alt="street view example lining up"
-src="/assets/articles/indonesia/plate.jpg"
-class="rounded-xl w-full h-96 object-cover" />
-<p class="mb-2"><b>License plates</b> on cars are <b>black</b> and have <b>three blocks</b> of <b>white digits</b> on them. Keep in mind: <b>Malaysia</b> only has <b>two</b>. </p>
-<img alt="street view example lining up"
-src="/assets/articles/indonesia/bikeplate.jpg"
-class="rounded-xl w-full h-96 object-cover" />
-<p class="mb-2">Front <b>license plates</b> on <b>motorcycles</b> are only seen in <b>Indonesia</b>, unlike any other country in <b>Southeast Asia</b>.</p>
-<br>
+<img
+    alt="street view example lining up"
+    src="/assets/articles/indonesia/plate.jpg"
+    class="rounded-xl w-full h-96 object-cover" />
+<p class="mb-4 mt-2 text-center">
+    <b>License plates</b> on cars are <b>black</b> and have <b>three blocks</b>
+    of <b>white digits</b> on them. <b>Malaysia</b> only has
+    <b>two</b>.
+</p>
+<img
+    alt="street view example lining up"
+    src="/assets/articles/indonesia/bikeplate.jpg"
+    class="rounded-xl w-full h-96 object-cover" />
+<p class="mb-2 mt-2 text-center">
+    Front <b>license plates</b> on <b>motorcycles</b> are seen in
+    <b>Indonesia</b>, unlike other countries in <b>Southeast Asia</b>.
+</p>
+<br />
 
 <h2 class="text-2xl font-bold mt-8 mb-2">Currency</h2>
-<img alt="street view example lining up"
-src="/assets/articles/indonesia/currency.jpg"
-class="rounded-xl w-full h-96 object-cover"/>
-<p class="mb-2">The official <b>Indonesian currency</b> is the <b>Rupiah</b> and is often seen on billboards as the short form <b>"Rp"</b>.</p>
-<br>
+<img
+    alt="street view example lining up"
+    src="/assets/articles/indonesia/currency.jpg"
+    class="rounded-xl w-full h-96 object-cover" />
+<p class="mb-2 mt-2 text-center">
+    The official <b>Indonesian currency</b> is the <b>Rupiah</b> and is often
+    seen on billboards as the short form <b>"Rp"</b>.
+</p>
+<br />
 
 <h2 class="text-2xl font-bold mt-8 mb-2">Driving side</h2>
-<Map region="SEAsia" interactive = false showLabels=true notHighlightedColor="#67999e" topoJsonName="south_east_asia"/>
-<p class="mb-2"><b>Indonesian traffic</b> drives on the <b>left</b>, unlike the <b>Philippines</b>, <b>Cambodia</b>, and <b>Laos</b>.</p>
-<br>
+<Map
+    region="South-East Asia"
+    interactive="false"
+    showLabels="true"
+    notHighlightedColor="#67999e"
+    topoJsonName="south_east_asia" />
+<p class="mb-2 mt-2 text-center">
+    <b>Indonesian traffic</b> drives on the <b>left</b>, unlike the
+    <b>Philippines</b>, <b>Cambodia</b>, and <b>Laos</b>.
+</p>
+<br />
 
 <h2 class="text-2xl font-bold mt-8 mb-2">Street names</h2>
 <img
-alt="street view example lining up"
-src="/assets/articles/indonesia/streetname.jpg"
-class="rounded-xl w-full object-cover" 
-/>
-<p class="mb-2">The <b>Indonesian word</b> for "<b>street</b>" is <b>"Jalan"</b> with the abbreviation <b>"Jl"</b>.</p>
-<br>
+    alt="street view example lining up"
+    src="/assets/articles/indonesia/streetname.jpg"
+    class="rounded-xl w-full object-cover" />
+<p class="mb-2 mt-2 text-center">
+    The <b>Indonesian word</b> for "<b>street</b>" is <b>"Jalan"</b> with the
+    abbreviation <b>"Jl"</b>.
+</p>
+<br />
 
 <h2 class="text-2xl font-bold mt-8 mb-2">Electricity poles</h2>
 <div class="flex space-x-4 w-full">
     <img
-    alt="street view example lining up"
-    src="/assets/articles/indonesia/pole(1).jpg"
-    class="rounded-xl w-full h-96 object-cover" />
+        alt="street view example lining up"
+        src="/assets/articles/indonesia/pole(1).jpg"
+        class="rounded-xl w-full h-96 object-cover" />
     <img
-    alt="street view example lining up"
-    src="/assets/articles/indonesia/pole(2).jpg"
-    class="rounded-xl w-full h-96 object-cover" />
+        alt="street view example lining up"
+        src="/assets/articles/indonesia/pole(2).jpg"
+        class="rounded-xl w-full h-96 object-cover" />
 </div>
-<p class="mb-2"><b>Electricity poles</b> in <b>Indonesia</b> are most commonly <b>round concrete poles</b> or thinner <b>metal poles</b>.</p>
+<p class=" mt-2 text-center">
+    <b>Electricity poles</b> in <b>Indonesia</b> are most commonly
+    <b>round concrete poles</b>
+    or thinner <b>metal poles</b>.
+</p>
 
-<br>
+<br />
 
 <h2 class="text-2xl font-bold mt-8 mb-2">Google car</h2>
 <div class="flex space-x-4 w-full">
     <img
-    alt="street view example lining up"
-    src="/assets/articles/indonesia/googlecar(1).jpg"
-    class="rounded-xl w-full h-96 object-cover" />
+        alt="street view example lining up"
+        src="/assets/articles/indonesia/googlecar(1).jpg"
+        class="rounded-xl w-full h-96 object-cover" />
     <img
-    alt="street view example lining up"
-    src="/assets/articles/indonesia/googlecar(2).jpg"
-    class="rounded-xl w-full h-96 object-cover" />
+        alt="street view example lining up"
+        src="/assets/articles/indonesia/googlecar(2).jpg"
+        class="rounded-xl w-full h-96 object-cover" />
 </div>
-<p class="mb-2"><b>Google cars</b> in <b>Indonesia</b> feature a variety of models, but most commonly you will see a <b>black car</b> with a <b>red brake light</b> or a <b>white car</b> with the <b>antenna visible</b>.</p>
-<br>
+<p class=" mt-2 text-center">
+    <b>Google cars</b> in <b>Indonesia</b> feature a variety of models, but most
+    commonly you will see a <b>black car</b> with a <b>red brake light</b> or a
+    <b>white car</b>
+    with the <b>antenna visible</b>.
+</p>
+<p class="italic mt-4">Written by IceB3r from the Discord Server</p>
