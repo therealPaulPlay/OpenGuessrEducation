98f0ff7b3c7346b9bcc95461cf007b7bdd31c1b6
༺♛ᴡᷫᴏͤʟͣғͬᴢᴋͭɪͪɴͤᴅ♛༻ <110046682+w0lfzk1n@users.noreply.github.com>
2025-01-11 15:19:11 +0100

    Add Finland page

--- src/routes/countries/learn/finland/+page.svelte
+++ src/routes/countries/learn/finland/+page.svelte
@@ -1,1 +1,24 @@
-<!-- Write or paste an article with HTML & Svelte here -->
+<script>
+    import ArticleEmbedPanorama from "$lib/components/ArticleEmbedPanorama.svelte";
+</script>
+
+<h2 class="text-2xl font-bold mb-2">Common locations</h2>
+<ul class="mb-2 list-disc list-inside">
+    <i>Add some here.</i>
+</ul>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">Strategies</h2>
+<ul class="mb-2 list-disc list-inside">
+    <li>Road signs typically end with <b>-katu</b> or <b>-tie</b>.</li>
+    <li>The presence of letters like <b>ä, ö</b>, but no å.</li>
+    <li>Lakes and forests dominate the landscape, offering natural clues.</li>
+    <li>Cultural markers such as sauna houses are prevalent in rural areas.</li>
+</ul>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">Features</h2>
+<ul class="mb-2 list-disc list-inside">
+    <li>Street signs are primarily in both Finnish and Swedish languages.</li>
+    <li>Parks filled with typical Finnish flora like birch trees and ferns.</li>
+    <li>Cultural markers such as traditional wooden houses can be found in rural areas.</li>
+    <li>Thousands of lakes and the untouched nature of Lapland are characteristic. The vegetation is similar to that in Sweden, with coniferous forests and birches in the north.</li>
+</ul>
