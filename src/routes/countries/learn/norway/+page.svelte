5fc72618a18435a0726b7b938f005227242d97db
༺♛ᴡᷫᴏͤʟͣғͬᴢᴋͭɪͪɴͤᴅ♛༻ <110046682+w0lfzk1n@users.noreply.github.com>
2025-01-11 15:09:48 +0100

    Add Norway Page

--- src/routes/countries/learn/norway/+page.svelte
+++ src/routes/countries/learn/norway/+page.svelte
@@ -1,1 +1,24 @@
-<!-- Write or paste an article with HTML & Svelte here -->
+<script>
+    import ArticleEmbedPanorama from "$lib/components/ArticleEmbedPanorama.svelte";
+</script>
+
+<h2 class="text-2xl font-bold mb-2">Common locations</h2>
+<ul class="mb-2 list-disc list-inside">
+    <i>Add some here.</i>
+</ul>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">Strategies</h2>
+<ul class="mb-2 list-disc list-inside">
+    <li>Road signs/names often end with <b>-gata</b> or <b>-veien.</li>
+    <li>The use of characters like <b>æ, ø, å</b>.</li>
+    <li>Diverse landscapes ranging from mountains to coastal areas.</li>
+    <li>Paved roads with distinctive markings indicating road types.</li>
+</ul>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">Features</h2>
+<ul class="mb-2 list-disc list-inside">
+    <li>Street signs are primarily in <b>Norwegian</b>.</li>
+    <li>Parks filled with typical Norwegian flora like <b>sparse coniferous trees</b>.</li>
+    <li>Cultural markers such as traditional stave churches can be found.</li>
+    <li>Known for breathtaking fjords and mountain landscapes. The treeless tundra with boggy soil, dwarf shrubs, mosses and lichens is typical.</li>
+</ul>
