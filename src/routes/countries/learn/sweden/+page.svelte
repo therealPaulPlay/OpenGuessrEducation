ef8a8f1e872136ffc2a64425aaa22bc3e213f46a
༺♛ᴡᷫᴏͤʟͣғͬᴢᴋͭɪͪɴͤᴅ♛༻ <110046682+w0lfzk1n@users.noreply.github.com>
2025-01-11 15:15:18 +0100

    Add Sweden page

--- src/routes/countries/learn/sweden/+page.svelte
+++ src/routes/countries/learn/sweden/+page.svelte
@@ -1,1 +1,24 @@
-<!-- Write or paste an article with HTML & Svelte here -->
+<script>
+    import ArticleEmbedPanorama from "$lib/components/ArticleEmbedPanorama.svelte";
+</script>
+
+<h2 class="text-2xl font-bold mb-2">Common locations</h2>
+<ul class="mb-2 list-disc list-inside">
+    <i>Add some here.</i>
+</ul>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">Strategies</h2>
+<ul class="mb-2 list-disc list-inside">
+    <li>Street signs often end with <b>-gatan</b> or <b>-vägen</b>.</li>
+    <li>Common use of <b>å, ä, ö</b> in road names.</li>
+    <li>Distinctive red and white road signs indicating speed limits.</li>
+    <li>Green landscapes with numerous lakes and forests.</li>
+</ul>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">Features</h2>
+<ul class="mb-2 list-disc list-inside">
+    <li>Street signs are predominantly in <b>Swedish</b>.</li>
+    <li>Public transport vehicles display <b>Swedish symbols</b>.</li>
+    <li>Parks filled with <b>birch trees</b> and beautiful flora.</li>
+    <li>Extensive forests and numerous lakes characterize the landscape. In the south there are still beech and oak stands.</li>
+</ul>
