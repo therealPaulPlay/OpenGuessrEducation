6c3b8a87b2d1799fcabe279388ecdc963e5ba4e5
༺♛ᴡᷫᴏͤʟͣғͬᴢᴋͭɪͪɴͤᴅ♛༻ <110046682+w0lfzk1n@users.noreply.github.com>
2025-01-12 15:54:23 +0100

    Implement requested changes by dev
    - Removed 'Add some here' and Locations part.
    - Highlighted important words.

--- src/routes/countries/learn/norway/+page.svelte
+++ src/routes/countries/learn/norway/+page.svelte
@@ -2,25 +2,20 @@
     import ArticleEmbedPanorama from "$lib/components/ArticleEmbedPanorama.svelte";
 </script>
 
-<h2 class="text-2xl font-bold mb-2">Common locations</h2>
-<ul class="mb-2 list-disc list-inside">
-    <i>Add some here.</i>
-</ul>
-
 <h2 class="text-2xl font-bold mt-8 mb-2">Strategies</h2>
 <ul class="mb-2 list-disc list-inside">
     <li>Road signs/names often end with <b>-gata</b> or <b>-veien.</li>
     <li>The use of characters like <b>æ, ø, å</b>.</li>
-    <li>Diverse landscapes ranging from mountains to coastal areas.</li>
-    <li>Paved roads with distinctive markings indicating road types.</li>
+    <li>Diverse landscapes ranging from <b>mountains</b> to <b>coastal areas.</b></li>
+    <li><b>Paved roads</b> with distinctive markings indicating road types.</li>
 </ul>
 
 <h2 class="text-2xl font-bold mt-8 mb-2">Features</h2>
 <ul class="mb-2 list-disc list-inside">
     <li>Street signs are primarily in <b>Norwegian</b>.</li>
     <li>Parks filled with typical Norwegian flora like <b>sparse coniferous trees</b>.</li>
-    <li>Cultural markers such as traditional stave churches can be found.</li>
-    <li>Known for breathtaking fjords and mountain landscapes. The treeless tundra with boggy soil, dwarf shrubs, mosses and lichens is typical.</li>
+    <li>Cultural markers such as <b>traditional stave churches</b> can be found.</li>
+    <li>Known for breathtaking fjords and mountain landscapes. The <b>treeless tundra with boggy soil, dwarf shrubs, mosses and lichens</b> is typical.</li>
 </ul>
 
 <p class="italic mt-4">Written by w0lfzk1nd from the Discord Server</p>
