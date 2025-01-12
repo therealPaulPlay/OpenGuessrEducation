dbf52ff3f11aacf0d87d8e38b93f1906a5837792
༺♛ᴡᷫᴏͤʟͣғͬᴢᴋͭɪͪɴͤᴅ♛༻ <110046682+w0lfzk1n@users.noreply.github.com>
2025-01-12 16:00:24 +0100

    Sweden - Implemented changes requested by dev
    - Removed 'Add some here' and Locations part.
    - Highlighted important words.

--- src/routes/countries/learn/sweden/+page.svelte
+++ src/routes/countries/learn/sweden/+page.svelte
@@ -2,25 +2,20 @@
     import ArticleEmbedPanorama from "$lib/components/ArticleEmbedPanorama.svelte";
 </script>
 
-<h2 class="text-2xl font-bold mb-2">Common locations</h2>
-<ul class="mb-2 list-disc list-inside">
-    <i>Add some here.</i>
-</ul>
-
 <h2 class="text-2xl font-bold mt-8 mb-2">Strategies</h2>
 <ul class="mb-2 list-disc list-inside">
     <li>Street signs often end with <b>-gatan</b> or <b>-vägen</b>.</li>
     <li>Common use of <b>å, ä, ö</b> in road names.</li>
-    <li>Distinctive red and white road signs indicating speed limits.</li>
-    <li>Green landscapes with numerous lakes and forests.</li>
+    <li>Distinctive <b>red and white road signs</b> indicating speed limits.</li>
+    <li>Green landscapes with <b>numerous lakes and forests.</b></li>
 </ul>
 
 <h2 class="text-2xl font-bold mt-8 mb-2">Features</h2>
 <ul class="mb-2 list-disc list-inside">
-    <li>Street signs are predominantly in <b>Swedish</b>.</li>
-    <li>Public transport vehicles display <b>Swedish symbols</b>.</li>
+    <li><b>Street signs</b> are predominantly <b>in Swedish</b>.</li>
+    <li><b>Public transport</b> vehicles display <b>Swedish symbols</b>.</li>
     <li>Parks filled with <b>birch trees</b> and beautiful flora.</li>
-    <li>Extensive forests and numerous lakes characterize the landscape. In the south there are still beech and oak stands.</li>
+    <li><b>Extensive forests and numerous lakes</b> characterize the landscape. In the <b>south</b> there are still <b>beech and oak</b> stands.</li>
 </ul>
 
 <p class="italic mt-4">Written by w0lfzk1nd from the Discord Server</p>
