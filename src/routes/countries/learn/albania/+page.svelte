eb6b2e08b7cb9b1a944a205f16db2234c1930fbe
w0lfzk1n <sn3x.royale@gmail.com>
2025-02-19 12:16:11 +0100

    Enhanced albania countrypage

--- src/routes/countries/learn/albania/+page.svelte
+++ src/routes/countries/learn/albania/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import ArticleEmbedPanorama from "$lib/components/ArticleEmbedPanorama.svelte";
+    import ArticleTip from "$lib/components/ArticleTip.svelte";
 </script>
 
 <h2 class="text-2xl font-bold mb-2">Common locations</h2>
@@ -15,30 +16,310 @@
     <li>
         <b>Shkodra:</b> Features <b>Rozafa Castle</b> and <b>Lake Shkodra</b>.
     </li>
+    <li>
+        <b>Lezhe:</b> Recognized by a <b>castle on top of a hill</b>, located <b>northwest</b>.
+    </li>
+    <li>
+        <b>Gjirokaster:</b> On a <b>west hillside</b> of a wide valley, located in bottom south.
+    </li>
 </ul>
 
-<ArticleEmbedPanorama src="https://www.google.com/maps/embed?pb=!4v1730050531195!6m8!1m7!1sCoiClDnLS-FzE6IQfQ1PcA!2m2!1d41.31649709254255!2d19.83984365534879!3f231.53028426853183!4f0.9043993737237344!5f0.7820865974627469" />
+<ArticleEmbedPanorama src="https://www.google.com/maps/embed?pb=!4v1730050531195!6m8!1m7!1sCoiClDnLS-FzE6IQfQ1PcA!2m2!1d41.31649709254255!2d19.83984365534879!3f231.53028426853183!4f0.9043993737237344!5f0.7820865974627469"/>
 <p class="text-center mt-2">A drop in Tirana.</p>
 
 <h2 class="text-2xl font-bold mt-8 mb-2">Strategies</h2>
 <ul class="mb-2 list-disc list-inside">
-    <li>Water heaters on <b>roofs</b>.</li>
+    <li>Water heaters on <b>roofs</b>, very common and almost on every single house.</li>
     <li>Two-storey houses, often <b>unfinished</b> or <b>run-down</b>.</li>
-    <li>
-        Bollards with a <b>red rectangle</b> in a black section; less common than
-        in Italy.
-    </li>
-    <li>Rifts in the sky, similar to <b>Senegal</b> and <b>Montenegro</b>.</li>
+    <li>Bollards with a <b>black top</b> and <b>red and grey reflectors</b>, white bottom; less common than in Italy.</li>
+    <li><b>Rifts at the top cameraview</b> due to bad image cropping, similar to <b>Montenegro</b> and rarely other countries.</li>
+    <li>A lot of <b>exposed red soil</b> (can also be found in Greece at Albanian border).</li>
 </ul>
 
-<ArticleEmbedPanorama src="https://www.google.com/maps/embed?pb=!4v1730051802424!6m8!1m7!1sVS3kXPSfmU8uR4VtfmZgZA!2m2!1d41.11137968057349!2d20.0819716044217!3f195.42901557805902!4f7.429448022263529!5f2.868877958143495" />
+<ArticleEmbedPanorama src="https://www.google.com/maps/embed?pb=!4v1730051802424!6m8!1m7!1sVS3kXPSfmU8uR4VtfmZgZA!2m2!1d41.11137968057349!2d20.0819716044217!3f195.42901557805902!4f7.429448022263529!5f2.868877958143495"/>
 <p class="text-center mt-2">Water heaters in Elbasan.</p>
+<br>
 
+<div class="flex flex-wrap justify-center w-full gap-6">
+    <div class="flex flex-col items-center w-full sm:w-1/2 lg:w-1/3">
+        <img alt="albania camera rift" src="/assets/articles/albania/albania_camerarift.jpg" class="rounded-xl w-full object-cover"/>
+        <p class="text-center mt-2">Rift at the top of camera view.</p>
+    </div>
+</div>
+<br>
+<div class="flex flex-wrap justify-center w-full gap-6">
+    <div class="flex flex-col items-center w-full sm:w-1/2 lg:w-1/3">
+        <img alt="albania riftmap" src="/assets/articles/albania/albania_riftmap.jpg" class="rounded-xl w-full object-cover"/>
+        <p class="text-center">
+            <a href="https://en.wikipedia.org/wiki/Highways_in_Albania#/media/File:Transport_in_Albania.png" target="_blank" class="text-blue-500 underline">Original Image [Wikipedia]</a>
+        </p>
+        <p class="text-center mt-2"><b>Blue:</b> Rifts and long antenna.<br><b>Red:</b> No rifts and short antenna.</p>
+    </div>
+</div>
+
+<br>
+<div class="flex flex-wrap justify-center w-full gap-4">
+    <div class="flex flex-col items-center w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+        <div class="h-48 w-full">
+            <img alt="albania guardrail" src="/assets/articles/albania/albania_guardrail.jpg" class="rounded-xl w-full h-full object-cover"/>
+        </div>
+        <p class="text-center mt-2">Albanian <b>flat and wide</b> guardrail ending.</p>
+    </div>
+    <div class="flex flex-col items-center w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+        <div class="h-48 w-full">
+            <img alt="albania chevron" src="/assets/articles/albania/albania_chevron.jpg" class="rounded-xl w-full h-full object-cover"/>
+        </div>
+        <p class="text-center mt-2">Albanian chevron also used by Italy, Greece, and Spain.</p>
+    </div>
+    <div class="flex flex-col items-center w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+        <div class="h-48 w-full">
+            <img alt="albania bollard" src="/assets/articles/albania/albania_bollard.jpg" class="rounded-xl w-full h-full object-cover"/>
+        </div>
+        <p class="text-center mt-2">Albanian bollard have a <b>black top</b> with a <b>grey and red reflector</b></p>
+    </div>
+    <div class="flex flex-col items-center w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+        <div class="h-48 w-full">
+            <img alt="albania chevron" src="/assets/articles/albania/albania_backsign.jpg" class="rounded-xl w-full h-full object-cover"/>
+        </div>
+        <p class="text-center mt-2"><b>Backside</b> of signs are often <b>painted black.</b></p>
+    </div>
+    <div class="flex flex-col items-center w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+        <div class="h-48 w-full">
+            <img alt="albania stopsign" src="/assets/articles/albania/albania_stopsign.jpg" class="rounded-xl w-full h-full object-cover object-top"/>
+        </div>
+        <p class="text-center mt-2">Albanian stopsign with <b>short endings on S</b>.</p>
+    </div>
+</div>
+
+<div class="flex flex-wrap justify-center w-full gap-4 mt-4">
+    <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+        <div class="h-48 w-full">
+            <img alt="albania utilitypole" src="/assets/articles/albania/albania_utilitypole1.jpg" class="rounded-xl w-full h-full object-top"/>
+        </div>
+    </div>
+    <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+        <div class="h-48 w-full">
+            <img alt="albania utilitypole" src="/assets/articles/albania/albania_utilitypole2.jpg" class="rounded-xl w-full h-full object-cover"/>
+        </div>
+    </div>
+    <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+        <div class="h-48 w-full">
+            <img alt="albania utilitypole" src="/assets/articles/albania/albania_utilitypole3.jpg" class="rounded-xl w-full h-full object-cover"/>
+        </div>
+    </div>
+</div>
+<p class="text-center mt-2">Albanian utilitypoles with <b>upside down triangles</b> or <b>horizontal bars</b>.</p>
+<br>
 <h2 class="text-2xl font-bold mt-8 mb-2">Features</h2>
 <ul class="mb-2 list-disc list-inside">
     <li>Street signs in <b>Albanian language</b>.</li>
     <li>Public transportation vehicles with <b>Albanian symbols</b>.</li>
-    <li>
-        Environmental markers like <b>olive trees</b> and <b>rugged terrain</b>.
-    </li>
+    <li>Environmental markers like <b>olive trees</b> and <b>rugged terrain</b>.</li>
+    <li><b>Driving</b> on the <b>right side</b> of the road.</li>
+    <li><b>Mercedes</b> is the most popular car brand. (Often a Sedan.)</li>
+    <li><b>Gen 3 coverage only</b>, limited to <b>main cities and roads</b>.</li>
 </ul>
+<br>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">Language</h2>
+<ul class="mb-2 list-disc list-inside">
+    <li>Uses <b>Latin alphabet</b> with <b>some unique letters</b>: "ë" (is very common) and "ç" (is less common).</li>
+    <li>Words may sound like gibberish or a fake language.</li>
+</ul>
+<div class="flex justify-center w-full">
+    <img alt="albania letters" src="/assets/articles/albania/albania_letters.jpg" class="rounded-xl w-1/2"/>
+</div>
+<p class="text-center mt-2">Albanian letters</p>
+<br>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">License Plates</h2>
+<ul class="mb-2 list-disc list-inside">
+    <li><b>Current</b> (since 2011): <b>Two blue stripes</b> on either side with <b>white center</b>.</li>
+    <li><b>Old</b> (pre-2011): <b>Red stripe on the left</b> with <b>white to the right</b> (unique in Europe).</li>
+</ul>
+<div class="flex justify-center w-full">
+    <img alt="albania new license plate" src="/assets/articles/albania/albania_license_current.jpg" class="w-1/2"/>
+</div>
+<p class="text-center mt-2">Albanian current license plate</p>
+<div class="flex justify-center w-full">
+    <img alt="albania old license plate" src="/assets/articles/albania/albania_license_old.jpg" class="w-1/2"/>
+</div>
+<p class="text-center mt-2">Albanian license plate before 2011</p>
+<br>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">Region Guessing</h2>
+<div class="overflow-x-auto">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Region</th>
+                <th>Description</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><b>Southwest</b></td>
+                <td>Sometimes <b>double white outer roadlines</b> on streets.</td>
+            </tr>
+            <tr>
+                <td><b>Extreme southeast on E86</b></td>
+                <td>Bollards similar to Turkey's (fully white with vertical red stripe at the top).</td>
+            </tr>
+            <tr>
+                <td><b>Mideast border, far south, and upper west border</b></td>
+                <td>Road lines with <b>one yellow dash along center</b>.</td>
+            </tr>
+            <tr>
+                <td><b>Bay of Vlore</b></td>
+                <td>Commonly seen on <b>SH4</b> with <b>Karabarun peninsula visible</b> on the other side.</td>
+            </tr>
+            <tr>
+                <td><b>North</b></td>
+                <td>Jagged mountains, especially <b>near Theth National Park</b> and <b>along Montenegro border</b>.</td>
+            </tr>
+            <tr>
+                <td><b>Northwest of Vlore</b></td>
+                <td><b>Aleppo pine forest</b> with coastal looks.</td>
+            </tr>
+            <tr>
+                <td><b>Center</b></td>
+                <td><b>Wide dried river</b> valley <b>moving southeast</b> along <b>SH71</b>.</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<br>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">Divided Highways</h2>
+<div class="overflow-x-auto">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>A1</td>
+                <td><b>Three guardrails</b>, located at the <b>upper west border</b>.</td>
+            </tr>
+            <tr>
+                <td>SH2 and SH4 (some sections)</td>
+                <td>Simple <b>concrete block barriers</b>, located <b>near midwest border</b>.</td>
+            </tr>
+            <tr>
+                <td>A3 and SH3 south of Tirana</td>
+                <td><b>Concrete blocks holding scenery</b> (plants/grass).</td>
+            </tr>
+            <tr>
+                <td>SH4</td>
+                <td><b>Basic double guardrails</b>, doesn't go through mountains, located <b>midwest border</b>.</td>
+            </tr>
+            <tr>
+                <td>E851</td>
+                <td><b>Basic double guardrails</b>, goes through mountains, <b>unique landscape and barriers</b>, located in <b>northeast</b>.</td>
+            </tr>
+            <tr>
+                <td>A2 north of Valore</td>
+                <td><b>Flat landscape</b> with <b>double guardrail</b> and <b>extra high fence</b> dividing the highway.</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">Road Classification</h2>
+<ul class="mb-2 list-disc list-inside">
+    <li><b>Motorway</b> (Rrugë Autostradale): <b>AX</b> (A1, A2,...), has green background.</li>
+    <li><b>State Road</b> (Rrugë Shtetërore): <b>SHX</b> (SH2, SH3,...), has blue background.</li>
+</ul>
+
+<div class="flex flex-col sm:flex-row justify-center w-full gap-4">
+    <div class="flex flex-col items-center w-full sm:w-1/2">
+        <div class="w-full h-64 sm:h-80 md:h-96">
+            <img alt="albania highways" src="/assets/articles/albania/albania_highways.jpg" class="rounded-xl w-full h-full object-contain"/>
+        </div>
+        <p class="text-center mt-2"><b>Green:</b> Motorways. <b>Blue:</b> State Roads </p>
+    </div>
+    <div class="flex flex-col items-center w-full sm:w-1/2">
+        <div class="w-full h-64 sm:h-80 md:h-96">
+            <img alt="albania coverage" src="/assets/articles/albania/albania_coverage.jpg" class="rounded-xl w-full h-full object-contain"/>
+        </div>
+        <p class="text-center mt-2">Albanian streetview coverage</p>
+    </div>
+</div>
+<br>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">Motorways</h2>
+<ul class="mb-2 list-disc list-inside">
+    <li><b>A1</b>: Dürres/Tirana to Kosovo (114km long), is part of E851 and E752.</li>
+    <li><b>A2</b>: Fier - Vlöre (46km long, 23.5km covered).</li>
+    <li><b>A3</b>: Tirana to Elbasan (31km long), is part of E852.</li>
+</ul>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">State Roads</h2>
+<div class="overflow-x-auto">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Length</th>
+        <th>Description</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><b>SH1</b></td>
+        <td>125km</td>
+        <td><b>2 lanes</b>. Tirana to the Montenegro-Albania border north of Shkodër.<br>Goes near the part where E762 merges with A1.</td>
+      </tr>
+      <tr>
+        <td><b>SH2</b></td>
+        <td>33km</td>
+        <td><b>4 lanes</b>. Tirana to Durrës.</td>
+      </tr>
+      <tr>
+        <td><b>SH3</b></td>
+        <td>151km</td>
+        <td><b>Mainly 2 lanes</b> (no coverage where 4 lanes are completed; under construction).<br>Tirana to the northernmost border with Greece.<br><b>Part of E852</b> from Elbasan to Lin (next to Macedonia) and <b>Part of E86</b> from Lin to the border.</td>
+      </tr>
+      <tr>
+        <td><b>SH4</b></td>
+        <td>212km</td>
+        <td><b>4 lanes</b> from Durrës to Fier, <b>2 lanes</b> for the rest.<br>Durrës to Greece. <b>Part of E853</b> from Fier to the border.</td>
+      </tr>
+      <tr>
+        <td><b>SH5</b></td>
+        <td>168km</td>
+        <td><b>2 lanes</b>. Shkodër to Kosovo (near Prizren).<br><b>Mountainous road</b> with many turns.</td>
+      </tr>
+      <tr>
+        <td><b>SH6</b></td>
+        <td>116km</td>
+        <td><b>2 lanes</b>. A1 near Laç to Peshkopi near Macedonia.<br><b>Mountainous road</b> with many turns.</td>
+      </tr>
+      <tr>
+        <td><b>SH7</b></td>
+        <td>77km</td>
+        <td>Rrogozhinë to Elbasan. Covered <b>only from Rrogozhinë to Papër</b>, south of Elbasan.</td>
+      </tr>
+      <tr>
+        <td><b>SH8</b></td>
+        <td>148km</td>
+        <td>Fier to Sarandë near Greece. Goes <b>near the Adriatic Sea</b>.</td>
+      </tr>
+      <tr>
+        <td><b>SH9</b></td>
+        <td>3km</td>
+        <td>SH3 near Lin to Montenegro. Is <b>part of E852</b>.</td>
+      </tr>
+      <tr>
+        <td><b>SH10</b></td>
+        <td>-</td>
+        <td>Currently <b>the A1</b>.</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<p class="italic mt-4">Enhanced by w0lfzk1nd and Nåchø17 from Discord</p>
