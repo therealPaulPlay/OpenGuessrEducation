a9cfc4e42eeeb28dfbd2599c410b86c7afb56e02
Luisa312 <luisa3129@outlook.com>
2026-02-15 16:13:51 +0100

    Guatemala car
    Added some stuff about the guatemala car

--- src/routes/countries/learn/guatemala/+page.svelte
+++ src/routes/countries/learn/guatemala/+page.svelte
@@ -1,1 +1,9 @@
-<!-- Write or paste an article with HTML & Svelte here -->
+<script>
+import ArticleEmbedPanorama from "$lib/components/ArticleEmbedPanorama.svelte";
+</script>
+
+<h2 class="text-2xl font-bold mt-8 mb-2">Guatemala Car</h2>
+<h3 class="text-xl font-bold mt-6 mb-1"></h3>
+<p class="mb-2">The Guatemala Car has a <b>roofrack</b> and the <b>rearview mirrors</b> are visible</p>
+<ArticleEmbedPanorama src="https://www.google.com/maps/embed?pb=!4v1771162125204!6m8!1m7!1sl-ugGQZ1XD1xPk7Gexmjjw!2m2!1d14.71342285209064!2d-90.58302005160473!3f146.41655312996292!4f-50.9303579673974!5f0.7820865974627469"></ArticleEmbedPanorama>
+
