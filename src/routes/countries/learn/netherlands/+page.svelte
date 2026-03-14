913d34a998919c40a2fa5cdc976a563c134f0aa4
WiJonathan <hoffmannjonathan53@gmail.com>
2026-03-14 13:58:05 +0100

    Update +page.svelte

--- src/routes/countries/learn/netherlands/+page.svelte
+++ src/routes/countries/learn/netherlands/+page.svelte
@@ -1,1 +1,14 @@
-<!-- Write or paste an article with HTML & Svelte here -->
+<script>
+</script>
+
+<p class="mb-2"><b>The Netherlands</b> is a country in Europe, located west of Germany and north of Belgium. </p>
+<p class="mb-2">The country is often <b>very flat</b>: 26% of the country even lies below sea level. Hills are almost exclusively found in the most southeastern province of the Netherlands, Limburg. On provincial roads and highways there are often bollards or signs with road numbers on them, making it easier to score high. In the west of the country sits the <b>Randstad</b>, a highly urbanized area. To the north, in the province of <b>Friesland</b>, street signs are bilingual (in Dutch and West Frisian) which can be an important clue.</p>
+<h2 class="text-2xl font-bold mt-8 mb-2"><b>Common road codes</b></h2>
+<ul class="mb-2 list-disc list-inside">
+<li><b>Ax(xx)</b>: Autosnelweg: highways (red)</li>
+<li><b>Nx(xx)</b>: Provinciale weg: Provincial roads (yellow)</li>
+<li><b>S1xx</b>: Stadsroute: Main arterial roads in large cities (white)</li>
+<li><b>R10x</b>: Recreatieve weg: Recreational roads only found in some parts of the Netherlands: near Nieuwvliet (Zeeland), Ommen (Overijssel), Schouwen (Zeeland), Spaarnwoude (Noord-Holland), Voorthuizen (Gelderland) en IJmuiden (Noord-Holland) (brown)</li>
+</ul>
+<p class="mb-2"><b>Note: x can be any number. S roads are between 100 and 130.</b></p>
+<p class="mb-2">Written by the user SwedishMountain</p>
