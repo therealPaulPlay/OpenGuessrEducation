47cd52141c2b5786ed81d2d1459ccc5f3c124c9e
nunaaaa-lab <noamparhipro@gmail.com>
2026-09-04 00:43:57 +0300

    Update +page.svelte

--- src/routes/countries/learn/israel/+page.svelte
+++ src/routes/countries/learn/israel/+page.svelte
@@ -1,1 +1,14 @@
 <!-- Write or paste an article with HTML & Svelte here -->
+<script>
+</script>
+
+<h2 class="text-2xl font-bold mt-8 mb-2"></h2>
+<p class="mb-2">Common locations in Israel:</p>
+<ul class="mb-2 list-disc list-inside">
+<li>Tel-Aviv: Beautiful beach coastline with more narrow streets.</li>
+<li>Jerusalem: known by the Jerusalem stone that surrounds the city.</li>
+<li>Haifa: More higher city with beautiful Mount Carmel.</li>
+<li>Be'er Sheva: Southern city with a dessert look, less green than the other cities.</li>
+</ul>
+<p class="text-center mt-2"></p>
+<p class="mb-2"><b>Israel known by the beautiful, changing landscapes from north to the south. Every city looks different from the other by street signs to the roads.</b></p>
